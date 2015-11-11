dc7a8bc8058d51bf90d02f1247b998cecbc99371
Michael Giovinco <msg5ez@virginia.edu>
2015-11-11 11:43:01 -0500

    added what Professor Sullivan added in class to catGo

--- catGo.py
+++ catGo.py
@@ -19,18 +19,22 @@ initState = 0
 # Display the state by drawing a cat at that x coordinate
 myimage = dw.loadImage("cat.bmp")
 
-
+# state -> image (IO)
 def updateDisplay(state):
     dw.fill(dw.black)
-    dw.draw(myimage, (state, width/2))
+    dw.draw(myimage, (state, height/2))
 
 
 # We'll update the state on each tick by incrementing the x stateinate
+# state -> state
 def updateState(state):
     return(state+1)
 
-# We'll terminate when the x stateinate reaches the screen edge
+# We'll terminate when the x coordinate reaches the screen edge
 
+# tells the simulation when it's over
+
+# state -> bool
 
 def endState(state):
     if (state >= width):
@@ -40,13 +44,13 @@ def endState(state):
 
 
 # For now we'll handle events just logging them to the console
-#
+# state -> event -> state
 def handleEvent(state, event):
     return(state)
 
 # Off we go! Start the cat at the left edge, and try for 30 FPS
-frameRate = 60
 initState = 0
+frameRate = 60
 # debug()
 rw.runWorld(initState, updateDisplay, updateState, handleEvent,
             endState, frameRate)
